2d188e02ce65d6d76c840ad95b36e7a6b664d0b7
taku26841397 <taku26841397@gmail.com>
2024-11-03 22:36:43 +0900

    更新

--- django/myproject/crud/models.py
+++ django/myproject/crud/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
     name=models.CharField(max_length=200)
@@ -6,3 +7,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    #新規作成・編集完了時のリダイレクト先
+    def get_absolute_url(self):
+        return reverse('list')
