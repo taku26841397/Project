c93dde5023c900d6ec7b0e326fc2b2a4bd57e34d
EG-LET-006\t.takusari <t.takusari@estrella.co.jp>
2024-11-06 18:29:36 +0900

    1

--- django/myproject/crud/models.py
+++ django/myproject/crud/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
 
-class Product(models.Model):
+class Category(models.Model):
     name=models.CharField(max_length=200)
-    price=models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
 
-    #新規作成・編集完了時のリダイレクト先
-    def get_absolute_url(self):
-        return reverse('list')
+class Product(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.PositiveIntegerField()
+    category=models.ForeignKey(Category,on_delete=models.CASCADE, db_column="category_id")
+    def __str__(self):
+        return self.name
--- django/myproject/crud/views.py
+++ django/myproject/crud/views.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView, ListView,DetailView
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from .models import Product
 from django.urls import reverse_lazy
+from django.urls import reverse
+
 class TopView(TemplateView):
     template_name = "top.html"
 
@@ -18,6 +20,11 @@ class ProductUpdateView(UpdateView):
     model=Product
     fields='__all__'
     template_name_suffix = '_update_form'
+
+
+    #新規作成・編集完了時のリダイレクト先
+    def get_absolute_url(self):
+        return reverse('list')
 
 class ProductDeleteView(DeleteView):
     model=Product
